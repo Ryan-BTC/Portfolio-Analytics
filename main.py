d1de4c6bc4a73e6fc68ab79baf0b404415b0ebdc
Ryan Pirotta <rpirotta@bluefort.eu>
2025-05-03 11:29:27 +0200

    Add portfolio analytics report for SuperTrend vs BTC (4H timeframe)
    - Integrated QuantStats to analyze SuperTrend strategy performance against BTC benchmark.
    
    - Downgraded and patched QuantStats due to compatibility issues with latest pandas version.
    
    - Resolved internal QuantStats errors (pivot) to enable successful report generation.

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello World")
+import quantstats as qs
+import pandas as pd
+
+if __name__ == '__main__':
+
+    timeframe = '4h'
+
+    dataframe = pd.read_csv(
+        f'SuperTrend_vs_BTC_Benchmark_{timeframe}_returns.csv',
+        usecols=['datetime', 'superTrend_return', 'btc_return'],
+        parse_dates=['datetime'],
+        date_format='%d/%m/%Y %H:%M')
+
+
+    dataframe['datetime'] = pd.to_datetime(dataframe['datetime'])
+    dataframe.set_index('datetime', inplace=True)
+
+    qs.reports.html(
+        dataframe['superTrend_return'],
+        dataframe['btc_return'],
+        title='SuperTrend VS Benchmark',
+        output='SuperTrend_vs_BTC.html',
+        download_filename=f'data/Analytics-Reports/SuperTrend_vs_BTC_{timeframe}.html'
+    )
