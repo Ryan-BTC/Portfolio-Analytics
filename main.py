54edaec4de3587d3339a4c9fee3578c96ec7b986
Ryan Pirotta <rpirotta@bluefort.eu>
2025-05-03 13:42:21 +0200

    Adding a for loop to iterate all timeframes at once

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ import pandas as pd
 
 if __name__ == '__main__':
 
-    timeframe = '4h'
+    timeframes = ['15m', '1h', '4h', '1d']
 
-    dataframe = pd.read_csv(
-        f'SuperTrend_vs_BTC_Benchmark_{timeframe}_returns.csv',
-        usecols=['datetime', 'superTrend_return', 'btc_return'],
-        parse_dates=['datetime'],
-        date_format='%d/%m/%Y %H:%M')
+    for timeframe in timeframes:
+        dataframe = pd.read_csv(
+            f'data/Combined-Returns/SuperTrend_vs_BTC_Benchmark_{timeframe}_returns.csv',
+            usecols=['datetime', 'superTrend_return', 'btc_return'],
+            parse_dates=['datetime'],
+            date_format='%d/%m/%Y %H:%M')
 
 
-    dataframe['datetime'] = pd.to_datetime(dataframe['datetime'])
-    dataframe.set_index('datetime', inplace=True)
+        dataframe['datetime'] = pd.to_datetime(dataframe['datetime'])
+        dataframe.set_index('datetime', inplace=True)
 
-    qs.reports.html(
-        dataframe['superTrend_return'],
-        dataframe['btc_return'],
-        title='SuperTrend VS Benchmark',
-        output='SuperTrend_vs_BTC.html',
-        download_filename=f'data/Analytics-Reports/SuperTrend_vs_BTC_{timeframe}.html'
-    )
+        qs.reports.html(
+            dataframe['superTrend_return'],
+            dataframe['btc_return'],
+            title='SuperTrend VS Benchmark',
+            output='SuperTrend_vs_BTC.html',
+            download_filename=f'data/Analytics-Reports/SuperTrend_vs_BTC_{timeframe}.html'
+        )
